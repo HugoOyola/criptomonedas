ccd9fb85e4ded7a66cb43f9d78de939ee191b1a1
Hugo Oyola <hugo.ham@outlook.com>
2024-04-12 12:28:30 -0500

    Agregar función consultarAPI para obtener datos de la API de criptomonedas

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 const criptomonedasSelect = document.querySelector('#criptomonedas');
 const monedaSelect = document.querySelector('#moneda');
 const formulario = document.querySelector('#formulario');
+const resultado = document.querySelector('#resultado');
 
 const objBusqueda = {
   moneda: '',
@@ -56,6 +57,9 @@ function submitFormulario(e) {
     mostrarAlerta('Ambos campos son obligatorios');
     return;
   }
+
+  // Consultar la API con los resultados
+  consultarAPI();
 }
 
 function mostrarAlerta(msg) {
@@ -74,4 +78,69 @@ function mostrarAlerta(msg) {
       divMensaje.remove();
     }, 3000);
   }
+}
+
+function consultarAPI(){
+  const { moneda, criptomoneda } = objBusqueda;
+
+  mostrarSpinner();
+
+  const url = `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${criptomoneda}&tsyms=${moneda}`;
+
+  fetch(url)
+    .then(respuesta => respuesta.json())
+    .then(cotizacion => {
+      mostrarCotizacionHTML(cotizacion.DISPLAY[criptomoneda][moneda]);
+    })
+}
+
+function mostrarCotizacionHTML(cotizacion) {
+
+  limpiarHTML();
+
+  const { PRICE, HIGHDAY, LOWDAY, CHANGEPCT24HOUR, LASTUPDATE } = cotizacion;
+
+  const precio = document.createElement('p');
+  precio.classList.add('precio');
+  precio.innerHTML = `El precio es: <span>${PRICE}</span>`;
+
+  const precioAlto = document.createElement('p');
+  precioAlto.innerHTML = `Precio más alto del día: <span>${HIGHDAY}</span>`;
+
+  const precioBajo = document.createElement('p');
+  precioBajo.innerHTML = `Precio más bajo del día: <span>${LOWDAY}</span>`;
+
+  const ultimasHoras = document.createElement('p');
+  ultimasHoras.innerHTML = `Variación últimas 24 horas: <span>${CHANGEPCT24HOUR}%</span>`;
+
+  const ultimaActualizacion = document.createElement('p');
+  ultimaActualizacion.innerHTML = `Última actualización: <span>${LASTUPDATE}</span>`;
+
+  resultado.appendChild(precio);
+  resultado.appendChild(precioAlto);
+  resultado.appendChild(precioBajo);
+  resultado.appendChild(ultimasHoras);
+  resultado.appendChild(ultimaActualizacion);
+}
+
+
+function limpiarHTML() {
+  while(resultado.firstChild) {
+    resultado.removeChild(resultado.firstChild);
+  }
+}
+
+function mostrarSpinner() {
+  limpiarHTML();
+
+  const spinner = document.createElement('div');
+  spinner.classList.add('spinner');
+
+  spinner.innerHTML = `
+    <div class="bounce1"></div>
+    <div class="bounce2"></div>
+    <div class="bounce3"></div>
+  `;
+
+  resultado.appendChild(spinner);
 }
